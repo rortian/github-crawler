75639b9b61e8f725336098836fc4a9d27177ab70
Jeremy Corbett <jeremy.lael@gmail.com>
2012-03-06 23:36:23 -0500

    One down

--- crawl.rb
+++ crawl.rb
@@ -5,3 +5,12 @@ require 'httpi'
 require 'neo4j'
 
 Neo4j::Config[:storage_path] = "neo4j"
+
+def followers(user)
+  r = HTTPI::Request.new
+  r.url = "http://github.com/api/v2/json/user/show/#{user}/followers"
+  res = HTTPI.get r
+  JSON.parse res.body
+end
+
+puts followers("rortian").inspect
