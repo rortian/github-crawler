30196ec5d6814322e2f744197cbf5cd948f4de5b
Jeremy Corbett <jeremy.lael@gmail.com>
2012-03-08 19:27:10 -0500

    Starting to feel a little more comfortable

--- crawl.rb
+++ crawl.rb
@@ -4,13 +4,29 @@ require 'json'
 require 'httpi'
 require 'neo4j'
 
+require 'pry'
+
 Neo4j::Config[:storage_path] = "neo4j"
 
-def followers(user)
-  r = HTTPI::Request.new
-  r.url = "http://github.com/api/v2/json/user/show/#{user}/followers"
-  res = HTTPI.get r
-  JSON.parse res.body
+class User
+  include Neo4j::NodeMixin
+
+  index :name
+
+  def followers(user)
+    r = HTTPI::Request.new
+    r.url = "http://github.com/api/v2/json/user/show/#{user}/followers"
+    res = HTTPI.get r
+    JSON.parse res.body
+  end
+
+  def following(user)
+    r = HTTPI::Request.new
+    r.url = "http://github.com/api/v2/json/user/show/#{user}/following"
+    res = HTTPI.get r
+    JSON.parse res.body
+  end
+
 end
 
-puts followers("rortian").inspect
+binding.pry
