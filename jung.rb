9bc8105e2edd11010568481307d3afc4357e01df
Jeremy Corbett <jeremy.lael@gmail.com>
2012-03-15 01:11:23 -0400

    Some imports

--- jung.rb
+++ jung.rb
@@ -9,4 +9,16 @@ jung_jars = File.join jung_dir,'*.jar'
 
 Dir.glob(jung_jars).each {|f| require f }
 
+module Jung
+
+  java_import 'edu.uci.ics.jung.graph.DirectedSparseGraph'
+
+  java_import 'edu.uci.ics.jung.algorithms.scoring.PageRank'
+
+  java_import 'edu.uci.ics.jung.graph.util.Pair'
+
+  java_import 'edu.uci.ics.jung.graph.util.EdgeType'
+
+end
+
 binding.pry
